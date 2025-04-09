4d7ca4ebc89057032129e03fc0efa41638338d31
Pedro Lucas  Reis de Oliveira Sousa <128660097+PedroReoli@users.noreply.github.com>
2025-04-09 15:57:23 -0300

    FrontEnd Beginning

--- src/main.tsx
+++ src/main.tsx
@@ -6,21 +6,6 @@ import { AuthProvider } from "./contexts/AuthContext"
 import "./styles/variables.css"
 import "./styles/globals.css"
 
-// css reutilizaveis abaixo
-import "./styles/passageiro.css"
-import "./styles/passageiro2.css"
-import "./styles/profile.css"
-import "./styles/settings.css"
-import "./styles/profile.css"
-import "./styles/components/alert.css"
-import "./styles/components/button.css"
-import "./styles/components/select.css"
-import "./styles/components/badge.css"
-import "./styles/components/avatar.css"
-import "./styles/components/card.css"
-import "./styles/components/input.css"
-import "./styles/components/modal.css"
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
